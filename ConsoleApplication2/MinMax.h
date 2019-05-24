59c7662c6ced28c97594830305ac4e488f9dbfbd
seacaboqn <seacaboqn@gmail.com>
2019-05-24 22:07:07 +0700

    Min Max

---
+++ ConsoleApplication2/MinMax.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _MY_MINMAX
+#define _MY_MINMAX
+
+void MinMax(int k, float a, float b, float c, float d, float e);
+void MinMax_1();
+void MinMax_2(float a, float b, float c);
+void MinMax_3(float a, float b, float  c, float  d);
+void MinMax_4(float a, float b, float c);
+void MinMax_5();
+void MinMax_6(float a, float b, float c, float d, float e);
+
+#endif#pragma once
