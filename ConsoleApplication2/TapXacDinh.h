552c98105db49f4fc5e82d7f7596b52e5f1b32a0
dinhhuuphuctrung <50943955+dinhhuuphuctrung@users.noreply.github.com>
2019-05-31 15:02:57 +0700

    Update TapXacDinh.h

--- ConsoleApplication2/TapXacDinh.h
+++ ConsoleApplication2/TapXacDinh.h
@@ -1,10 +1,14 @@
 #ifndef _MY_TXD
 #define _MY_TXD
 
-void TapXacDinh(int k, float a, float b, float c, float d, float e);
-void TapXacDinh_1();
-void TapXacDinh_2(float a, float b, float c, float d);
-void TapXacDinh_3(float a, float b, float c, float d, float e);
+//k là loại hàm số; a, b, c, d, e là các hệ số của hàm số đó (nếu có).
+void TapXacDinh(int k, float a, float b, float c, float d, float e); 
+//Xuất ra tập xác định là R
+void TapXacDinh_1(); 
+//Tập xác định của hàm phân thức hữu tỉ (ax+b)/(cx+d)
+void TapXacDinh_2(float a, float b, float c, float d); 
+//Tập xác định của hàm phân thức (ax^2+bx+c)/(dx+e)
+void TapXacDinh_3(float a, float b, float c, float d, float e); 
 void TapXacDinh_4();
 void TapXacDinh_5();
 void TapXacDinh_11(float a);
